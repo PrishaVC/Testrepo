c1e81ae7711c90bbff02da406817e79c04746c62
vasan <vasan@Vasant>
2023-12-25 11:27:55 +0530

    ok

--- com.iso.project/src/com/iso/tests/LoginTest.java
+++ com.iso.project/src/com/iso/tests/LoginTest.java
@@ -4,24 +4,22 @@ import org.testng.annotations.Test;
 
 import com.iso.pages.LoginPage;
 
-public class LoginTest 
-{
+public class LoginTest {
+	
 	@Test
-	public void loginTest() throws Exception
+	public void loginTest() throws Exception 
 	{
-		LoginPage lp=new LoginPage();
-		
-		
+		LoginPage lp = new LoginPage();
+
 		lp.enterEmail();
 		lp.enterPassword();
 		Thread.sleep(5000);
-		
+
 		lp.clickLoginButton();
 		Thread.sleep(5000);
-		
+
 		System.out.println("ISO Application Login Successfully");
-		
+
 	}
-	
-	
+
 }
